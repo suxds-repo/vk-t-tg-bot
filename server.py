32b4611ceaf135bce07743035b304c80584e56bf
suxds-repo <suxds@yandex.ru>
2025-12-01 07:58:33 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,19 +1,10 @@
-from fastapi import FastAPI, Request
-from bot_webhook import dispatcher, check_vk_posts
-from telegram import Update
+from fastapi import FastAPI
+import asyncio
+from bot import check_vk_posts
 
 app = FastAPI()
 
-# Telegram Webhook
-@app.post("/webhook")
-async def telegram_webhook(req: Request):
-    data = await req.json()
-    update = Update.de_json(data, dispatcher.bot)
-    await dispatcher.process_update(update)
-    return {"ok": True}
-
-# CRON / Ping endpoint
 @app.get("/check")
-async def cron_check():
+async def check():
     await check_vk_posts()
     return {"status": "ok"}
